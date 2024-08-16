a71735c87d9ea4eda7aa91f1d211e47b9fc5c847
Joey <joey.kennedy1@gmail.com>
2024-08-16 15:42:12 -0400

    migration to recipe_scrapers v15

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import sys
-
-
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
+}
 def eprint(*args, **kwargs):
     """
     Print to standard error for the console.
